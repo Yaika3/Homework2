1ddeade4be5c9abdcaf7e01a67c16b60dd1e2d4e
max <kabinidi@mail.ru>
2022-10-18 17:04:20 +0300

    answer 4

--- src/Main.java
+++ src/Main.java
@@ -21,6 +21,14 @@ public class Main {
         paper =paper - 7639;
         System.out.println(paper);
 
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend - 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+
+
 
 
 
